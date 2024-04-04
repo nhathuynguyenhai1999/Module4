9145d9447a7f1b1cc8de47046947fe04de1794d3
nhathuynguyenhai1999 <nhathuynguyenhai@gmail.com>
2024-04-04 11:41:49 +0700

    remove servlet and upload 04-04-2024

--- Currency/src/main/java/com/cg/module4/currency/controller/CurrencyConverterController.java
+++ Currency/src/main/java/com/cg/module4/currency/controller/CurrencyConverterController.java
@@ -1,6 +1,5 @@
 // CurrencyConverterController.java
 package com.cg.module4.currency.controller;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
--- Dictionary/src/main/java/com/cg/module4/dictionary/Controller/DictionaryController.java
+++ Dictionary/src/main/java/com/cg/module4/dictionary/Controller/DictionaryController.java
@@ -1,4 +1,4 @@
-package com.cg.module4.dictionary.Model;
+package com.cg.module4.dictionary.Controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
