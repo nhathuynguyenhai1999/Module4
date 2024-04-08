0e264ca99ab889786394594947c314fa087dd4c1
nhathuynguyenhai1999 <nhathuynguyenhai@gmail.com>
2024-04-08 12:33:22 +0700

    upload 08-04-2024

---
+++ Form-DataBinding/spring-user-tutorial/src/main/java/com/codegym/module4/springusertutorial/Controller/UserController.java
@@ -0,0 +1,30 @@
+package com.codegym.module4.springusertutorial.Controller;
+import com.codegym.module4.springusertutorial.DAO.UserDAO;
+import com.codegym.module4.springusertutorial.Model.Login;
+import com.codegym.module4.springusertutorial.Model.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
+@Controller
+public class UserController {
+    @GetMapping("/home")
+    public ModelAndView home() {
+        ModelAndView modelAndView = new ModelAndView("home");
+        modelAndView.addObject("login", new Login());
+        return modelAndView;
+    }
+    @PostMapping("/login")
+    public ModelAndView login(@ModelAttribute("login") Login login){
+        User user = UserDAO.checkLogin(login);
+        ModelAndView modelAndView;
+        if(user == null){
+            modelAndView = new ModelAndView("error");
+        } else {
+            modelAndView = new ModelAndView("user");
+            modelAndView.addObject("user", user);
+        }
+        return modelAndView;
+    }
+}
