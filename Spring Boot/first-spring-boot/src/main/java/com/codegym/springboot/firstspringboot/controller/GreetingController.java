eb6a680341f57637dcda90ed8e77573c73ac42f2
nhathuynguyenhai1999 <nhathuynguyenhai@gmail.com>
2024-04-11 14:16:49 +0700

    fix,add, checked and upload 11-04-2024

--- Spring Boot/first-spring-boot/src/main/java/com/codegym/springboot/firstspringboot/controller/GreetingController.java
+++ Spring Boot/first-spring-boot/src/main/java/com/codegym/springboot/firstspringboot/controller/GreetingController.java
@@ -1,4 +1,4 @@
-package com.codegym.springboot.firstspringboot;
+package com.codegym.springboot.firstspringboot.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
---
+++ Spring Boot/first-spring-boot/src/main/java/com/codegym/springboot/firstspringboot/controller/HandlerController.java
@@ -0,0 +1,26 @@
+package com.codegym.springboot.firstspringboot.controller;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HandlerController implements ErrorController {
+    @RequestMapping("/error")
+    public String handleError(HttpServletRequest httpServletRequest){
+        Object status = httpServletRequest.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+        if(status != null){
+            int statusCode = Integer.parseInt(status.toString());
+            if(statusCode == HttpStatus.NOT_FOUND.value()){
+                return "error_404";
+            }
+            else if(statusCode == HttpStatus.INTERNAL_SERVER_ERROR.value()){
+                return "error_500";
+            }
+        }
+        return "error";
+    }
+}
