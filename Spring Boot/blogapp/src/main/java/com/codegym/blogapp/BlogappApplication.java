464a09d0677a0bb50cef3f3d32051beb4a49aab0
nhathuynguyenhai1999 <nhathuynguyenhai@gmail.com>
2024-04-12 19:28:47 +0700

    upload 7:28 CH | 12-04-2024

--- Spring Boot/blogapp/src/main/java/com/codegym/blogapp/BlogappApplication.java
+++ Spring Boot/blogapp/src/main/java/com/codegym/blogapp/BlogappApplication.java
@@ -1,13 +1,46 @@
 package com.codegym.blogapp;
 
+import com.codegym.blogapp.model.Category;
+import com.codegym.blogapp.model.Post;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+import jakarta.persistence.Query;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.List;
 
 @SpringBootApplication
 public class BlogappApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(BlogappApplication.class, args);
+
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("my-persistence-unit");
+        EntityManager em = emf.createEntityManager();
+
+        // Bước 1: Truy vấn danh mục
+        Query categoryQuery = em.createQuery("select c from Category c");
+        List<Category> categories = categoryQuery.getResultList();
+
+        // Bước 2: Phân trang bài viết
+        Query postQuery = em.createQuery("select p from Post p", Post.class);
+        postQuery.setFirstResult(0);
+        postQuery.setMaxResults(10);
+        List<Post> posts = postQuery.getResultList();
+
+        // Bước 3: Sắp xếp bài viết theo tiêu đề
+        Query sortedPostQuery = em.createQuery("select p from Post order by p.title", Post.class);
+        List<Post> sortedPosts = sortedPostQuery.getResultList();
+
+        // Bước 4: Thực hiện join
+        Query joinQuery = em.createQuery("select p from Post p join fetch p.category", Post.class);
+        List<Post> postWithCategory = joinQuery.getResultList();
+
+        em.close();
+        emf.close();
     }
 
 }
---
+++ Spring Boot/blogapp/src/main/java/com/codegym/blogapp/model/Category.java
@@ -0,0 +1,33 @@
+package com.codegym.blogapp.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+
+@Getter
+@Entity
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+    public Category() {
+
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Category(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+}
