0fb967c926bd5f3534ec45e6235798252b43c5c9
nhathuynguyenhai1999 <nhathuynguyenhai@gmail.com>
2024-05-03 15:56:39 +0700

    upload message

---
+++ Spring Boot/blogapp/src/main/java/com/codegym/blogapp/security/SecurityConfig.java
@@ -0,0 +1,35 @@
+package com.codegym.blogapp.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+
+
+//@Configuration
+//@EnableWebSecurity
+//public class SecurityConfig extends WebSecurityConfigurerAdapter  {
+//
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//        http.authorizeRequests()
+//                .antMatchers("/public/**").permitAll() // Các URL công khai
+//                .anyRequest().authenticated() // Các URL khác yêu cầu xác thực
+//                .and()
+//                .formLogin() // Sử dụng form login
+//                .loginPage("/login") // Trang đăng nhập
+//                .permitAll()
+//                .and()
+//                .logout() // Xử lý đăng xuất
+//                .permitAll();
+//    }
+//
+//    @Autowired
+//    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+//        auth.inMemoryAuthentication()
+//                .withUser("user").password("{noop}password").roles("USER"); // Tạo một người dùng trong bộ nhớ để kiểm tra
+//    }
+//}
