6138c0a62147ef2ee4a56fd27c18a35d8ce98421
Nguyễn Hài Nhật Huy <129018441+nhathuynguyenhai1999@users.noreply.github.com>
2024-05-15 15:10:32 +0700

    add | fix | upload message

--- SpringMVC/custom/src/main/java/com/cg/module4/custom/CustomerController.java
+++ SpringMVC/custom/src/main/java/com/cg/module4/custom/CustomerController.java
@@ -1,33 +1,66 @@
 package com.cg.module4.custom;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
 import java.util.List;
 @Controller
 @RequestMapping("/customers")
 public class CustomerController{
-    @Autowired
-    private ICustomerService customerService;
-    @GetMapping
-    public ModelAndView showList(){
-        ModelAndView modelAndView = new ModelAndView("list");
-        List<Customer> customers = customerService.findAll();
-        modelAndView.addObject("customers", customers);
-        return modelAndView;
+    private final ICustomerService customerService = new CustomerService();
+
+    @GetMapping("")
+    public String index(Model model) {
+
+        List<Customer> customerList = customerService.findAll();
+        model.addAttribute("customers", customerList);
+        return "/index";
     }
-    @GetMapping("/{id}")
-    public ModelAndView showInformation(@PathVariable Long id) {
-        ModelAndView modelAndView = new ModelAndView("info");
-        Customer customer = customerService.findById(id);
-        modelAndView.addObject("customer", customer);
-        return modelAndView;
+
+    @GetMapping("/create")
+    public String create(Model model) {
+        model.addAttribute("customer", new Customer());
+        return "/create";
     }
-    @PostMapping
-    public String updateCustomer(@RequestParam Long id,
-                                 @RequestParam String name,
-                                 @RequestParam String email,
-                                 @RequestParam String address) {
+
+    @PostMapping("/save")
+    public String save(Customer customer) {
+        customer.setId((int) (Math.random() * 10000));
+        customerService.save(customer);
         return "redirect:/customers";
+    }
+
+    @GetMapping("/{id}/edit")
+    public String update(@PathVariable int id, Model model) {
+        model.addAttribute("customer", customerService.findById(id));
+        return "/update";
+    }
+
+    @PostMapping("/update")
+    public String update(Customer customer) {
+        customerService.update(customer.getId(), customer);
+        return "redirect:/customers";
+    }
+
+    @GetMapping("/{id}/delete")
+    public String delete(@PathVariable int id, Model model) {
+        model.addAttribute("customer", customerService.findById(id));
+        return "/delete";
+    }
+
+    @PostMapping("/delete")
+    public String delete(Customer customer, RedirectAttributes redirect) {
+        customerService.remove(customer.getId());
+        redirect.addFlashAttribute("success", "Removed customer successfully!");
+        return "redirect:/customers";
+    }
+
+    @GetMapping("/{id}/view")
+    public String view(@PathVariable int id, Model model) {
+        model.addAttribute("customer", customerService.findById(id));
+        return "/index";
     }
 }
